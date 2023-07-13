f610e2e88cc0f872fb7e36bac4de94df08132da5
syafi_tess <syafi_tess@outlook.com>
2023-07-13 16:51:52 +0800

    add loading when creating post

--- src/Pages/CreatePost/CreatePost.js
+++ src/Pages/CreatePost/CreatePost.js
@@ -1,9 +1,9 @@
 import "./CreatePost.scss";
 import uploadIcon from "../../Assets/Icons/upload.svg";
-import { ChangeEvent,useRef, useState, useEffect } from "react";
+import { ChangeEvent, useRef, useState, useEffect } from "react";
 import PostSvc from "../../Services/PostSvc";
 import spinner from "../../Assets/Icons/Spinner.svg";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 import utils from "../../Utils/utils";
 
 function CreatePost() {
@@ -12,84 +12,98 @@ function CreatePost() {
   const [postDescription, setPostDescription] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(()=>{
-    if(selectedImages.length >0){
+  useEffect(() => {
+    if (selectedImages.length > 0) {
       setSelectedImage(selectedImages[selectedImages.length - 1]);
     }
 
-    if(selectedImages.length === 0){
-      setSelectedImage(null)
+    if (selectedImages.length === 0) {
+      setSelectedImage(null);
     }
- 
-  },[selectedImages])
+  }, [selectedImages]);
 
   const handleFileChange = (e) => {
     if (e.target.files) {
       if (e.target.files && e.target.files.length > 0) {
-        setSelectedImages(selectedImages => [...selectedImages, e.target.files[0]])
+        setSelectedImages((selectedImages) => [
+          ...selectedImages,
+          e.target.files[0],
+        ]);
       }
     }
   };
 
-  const clearPrev = (e)=>{
-    e.currentTarget.value = null
-  }
+  const clearPrev = (e) => {
+    e.currentTarget.value = null;
+  };
 
   const handleUploadClick = async () => {
     setIsLoading(true);
     const username = localStorage.getItem("username");
     const userId = localStorage.getItem("userId");
     let formData = new FormData();
-    formData.append("PostId",0);
-    formData.append("UserId",userId);
-    formData.append("Description",postDescription);
-    formData.append("Likes",0);
-    formData.append("images",[]);
+    formData.append("PostId", 0);
+    formData.append("UserId", userId);
+    formData.append("Description", postDescription);
+    formData.append("Likes", 0);
+    formData.append("images", []);
 
     for (const image of selectedImages) {
-      formData.append("formFiles",image)
+      formData.append("formFiles", image);
     }
     const res = await PostSvc().AddPost(formData);
 
     setIsLoading(false);
     Swal.fire({
-      icon: 'success',
-      title: 'Success!',
-      text: 'Post has been created',
-      customClass: utils().GetSwalCustomClass()
-    })
+      icon: "success",
+      title: "Success!",
+      text: "Post has been created",
+      customClass: utils().GetSwalCustomClass(),
+    });
 
     setSelectedImage(null);
     setSelectedImages([]);
     setPostDescription("");
   };
 
-  function removeImg (i){
-    setSelectedImages(selectedImages.filter((img,index)=> index !== i ))
-    
+  function removeImg(i) {
+    setSelectedImages(selectedImages.filter((img, index) => index !== i));
   }
 
   return (
     <div className="CP-Container">
       <div className="CP-ImageUploader">
-        <label htmlFor="file-upload"  className="CP-ImageContainer custom-file-upload"
-          style={{ backgroundImage: `url(${uploadIcon})`}}> 
-          {
-            selectedImage &&(
-              <img className="mainPreviewImg" alt="preview" src={URL.createObjectURL(selectedImage)}></img>
-            ) 
-          }
+        <label
+          htmlFor="file-upload"
+          className="CP-ImageContainer custom-file-upload"
+          style={{ backgroundImage: `url(${uploadIcon})` }}
+        >
+          {selectedImage && (
+            <img
+              className="mainPreviewImg"
+              alt="preview"
+              src={URL.createObjectURL(selectedImage)}
+            ></img>
+          )}
         </label>
-        <input id="file-upload" type="file" onChange={handleFileChange} onClick={clearPrev} />
+        <input
+          id="file-upload"
+          type="file"
+          onChange={handleFileChange}
+          onClick={clearPrev}
+        />
         <div className="CP-ImagePreviewGallery">
-          {selectedImages.map((img,i)=>{
-            return(
-              <img onClick={()=>removeImg(i)} key={i} src={URL.createObjectURL(img)} className="imgPreview" alt="preview"></img>
-
+          {selectedImages.map((img, i) => {
+            return (
+              <img
+                onClick={() => removeImg(i)}
+                key={i}
+                src={URL.createObjectURL(img)}
+                className="imgPreview"
+                alt="preview"
+              ></img>
             );
-
           })}
-  
         </div>
       </div>
       <div className="CP-PostDetail">
@@ -100,7 +114,17 @@ function CreatePost() {
           name="postDesc"
           onChange={(e) => setPostDescription(e.target.value)}
         />
-        <input className="PostBtn" onClick={handleUploadClick} type="button" value="Post" />
+        {isLoading ? (
+          <img src={spinner} alt="loading" />
+        ) : (
+          <input
+          className="PostBtn"
+          onClick={handleUploadClick}
+          type="button"
+          value="Post"
+        />
+        )}
+
       </div>
     </div>
   );
