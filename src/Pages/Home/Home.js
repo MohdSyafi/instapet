26bc4765a8f3569d7390d6e32d6c712b205453aa
MohdSyafi <syafimohamad13@gmail.com>
2023-07-17 08:52:40 +0800

    add loading in profile and home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,11 +3,13 @@ import { useNavigate } from "react-router";
 import "./Home.scss";
 import Post from "../../Components/Post/Post";
 import PostSvc from "../../Services/PostSvc";
+import spinner from "../../Assets/Icons/Spinner.svg"
 
 function Home() {
   const navigate = useNavigate();
   const [authenticated, setauthenticated] = useState(false);
   const [postList, setpostList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const loggedInUser = localStorage.getItem("authenticated");
@@ -22,28 +24,35 @@ function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [authenticated]);
 
-  useEffect(()=>{
+  useEffect(() => {
 
     const fetchPost = async () => {
+      setIsLoading(true);
       const userId = localStorage.getItem("userId");
       let posts = await PostSvc(userId).getFollowingPosts(userId);
       setpostList(posts);
-    } 
+      setIsLoading(false);
+    }
 
-    fetchPost()    
-    // make sure to catch any error
-    .catch(console.error);;
+    fetchPost()
+      // make sure to catch any error
+      .catch(console.error);;
 
-  },[])
+  }, [])
 
   return (
-      <div className="HomeContainer"> 
-        <div className="AllPostsContainer"> 
-          {postList && postList.map((p, index) => (
-            <Post key={index} data={p} />
-          ))}
-        </div> 
+    <div className="HomeContainer">
+      {isLoading &&
+        <div className="loadingContainer">
+          <img src={spinner} className="loading" alt="loading" />
+        </div>
+      }
+      <div className="AllPostsContainer">
+        {postList && postList.map((p, index) => (
+          <Post key={index} data={p} />
+        ))}
       </div>
+    </div>
 
   );
 }
--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -2,26 +2,37 @@ import placeholderPic from "../../Assets/Images/profilePic.png";
 import "../Profile/Profile.scss";
 import { useEffect, useState } from "react";
 import ProfileSvc from "../../Services/ProfileSvc";
+import spinner from "../../Assets/Icons/Spinner.svg"
+
 function Profile({ userId }) {
   const [profileData, setProfileData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchProfile = async () => {
+
+      setIsLoading(true);
       const userId = localStorage.getItem("userId");
       let profile = await ProfileSvc().GetProfile(userId);
       setProfileData(profile);
+      setIsLoading(false);
     };
 
-    let profileUserID = localStorage.getItem("userId");
-        fetchProfile(profileUserID)    
-    // make sure to catch any error
-    .catch(console.error);
+    fetchProfile()
+      // make sure to catch any error
+      .catch(console.error);
   }, []);
 
   return (
     <>
-      {profileData && (
-          <div className="ProfileContainer">
+      <div className="ProfileContainer">
+        {isLoading &&
+          <div className="loadingContainer">
+            <img src={spinner} className="loading" alt="loading" />
+          </div>
+        }
+        {profileData && (
+          <>
             <div className="ProfileTop">
               <div className="ProfilePic">
                 <img src={placeholderPic} alt="profile" />
@@ -55,15 +66,18 @@ function Profile({ userId }) {
             </div>
             <div className="ProfileBottom">
               <div className="PostGrid">
-                {profileData.postAndImages.map((post,i)=>
-                    <div key={i} className="Tile">
-                        {<img src={post.images[0].location} alt="post" />}
-                    </div>
+                {profileData.postAndImages.map((post, i) =>
+                  <div key={i} className="Tile">
+                    {<img src={post.images[0].location} alt="post" />}
+                  </div>
                 )}
               </div>
             </div>
-          </div>      
-      )}
+          </>
+        )}
+
+      </div>
+
     </>
   );
 }
